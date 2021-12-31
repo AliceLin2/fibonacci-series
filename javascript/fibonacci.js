dc7272b6c3b9175a79420f464eb84e9d5f87d5e8
shu Lin <bbqlinshu@gmail.com>
2021-12-31 15:30:03 -0500

    Completed assignment

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,5 +1,12 @@
 function fibonacci(num) {
   // type your code here
+  if(num === 0){
+    return 0
+  }else if(num === 1){
+      return 1
+  }else{
+    return fibonacci(num-2)+fibonacci(num-1)
+  }
 }
 
 if (require.main === module) {
@@ -16,9 +23,23 @@ if (require.main === module) {
 
   console.log("Expecting: 55");
   console.log("=>", fibonacci(10));
+
+  console.log("");
+
+  console.log("Expecting: 377");
+  console.log("=>", fibonacci(14));
 }
 
 module.exports = fibonacci;
 
 // Please add your pseudocode to this file
+/* 0,1,1,2,3,5,8,13,21,34,55,89,144,233,377 
+  if num is zero:
+    return zero,
+  else if num is 1:
+    return 1
+  else:
+    using recursion to return fibonacci(num-2)+fibonacci(num-1) until n-2 = 0
+*/
 // And a written explanation of your solution
+
